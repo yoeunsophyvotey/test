ce2b89b4d842b094c56a701383178c345261a9e0
Giselle1904 <yoeunsophyvotey@gmail.com>
2026-06-15 16:37:05 +0700

    Update project and File

---
+++ javascript/index.js
@@ -0,0 +1,32 @@
+// let id;
+// let name, gender;
+
+// id=44;
+// name = "Votey";
+// gender= "Male";
+
+// console.log("ID : "+id);
+// console.log("Name : "+name);
+
+let num1, num2;
+
+// ==============================
+
+let id;
+let name;
+let price;
+let qty, discount;
+let total_price, payment;
+
+id = 10;
+name = "Keyboard";
+price = 50.00;
+qty = 1;
+discount = 5.00;
+total_price=(price*qty)-discount;
+
+
+
+
+
+
